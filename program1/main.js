3154689199b9007a346c189a92bde9c72651ff49
Enrique Austin <34680513+EnriqueAustin@users.noreply.github.com>
2022-08-22 16:20:36 +0200

    create file not orking

--- program1/main.js
+++ program1/main.js
@@ -1,3 +1,59 @@
+var name;
+var university;
+var githubusername;
+
+var dice1;
+var dice2;
+
+var arrCatagories = [
+    'Best Consumer Solution',
+    'Best Enterprise Solution',
+    'Best African Solution',
+    'Most Innovative Solution',
+    'Best Gaming Solution',
+    'Best Health Solution',
+    'Best Agricultural Solution',
+    'Best Educational Solution',
+    'Best Financial Solution',
+    'Best Hackathon Solution',
+    'Best South African Solution',
+    'Best Campus Cup Solution',
+];
+
 function logInfo() {
-    alert("Hello World");
+    name = prompt('Enter name');
+    university = prompt('Enter university');
+    githubusername = prompt('Enter Github Username');
+
+    console.log('My name is ' + name + ', a student from ' + university + ' and my Github username is ' + githubusername);
 } 
+
+function rollDice() {
+    var total;
+    var winner;
+
+    dice1 = Math.floor(Math.random() * 6) + 1;
+    dice2 = Math.floor(Math.random() * 6) + 1;
+
+    console.log(dice1 + ', ' + dice2);
+
+    total = dice1 + dice2;
+    winner = arrCatagories[total];
+
+    console.log(winner);
+    alert(winner);
+}
+
+function createfile() {
+    const fs = require('fs')
+  
+    let data = "Learning how to write in a file."
+  
+    fs.writeFile('Output.txt', data, err => {
+
+        if (err) {
+            console.err;
+            return;
+        }
+    });
+}
